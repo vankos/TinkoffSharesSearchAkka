276ac4fe02ce0c10cb04127b0674afb168642b10
Vanko <vano111ru@yandex.ru>
2020-06-20 15:08:24 +0300

    добавлен рейтинг из Zacks

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,40 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Tinkoff.Trading.OpenApi.Network;
 using Tinkoff.Trading.OpenApi.Models;
-using System.Collections.ObjectModel;
+using System.Data;
+using System.Net;
+using System.Text.RegularExpressions;
+using Python.Runtime;
 
 namespace Tinkoff
 {
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        const string saveFilePath = "savedData.dat";
-        Context context = null;
-        SaveData savedata= new SaveData();
-        bool IsInitializing=true;
+        private const string saveFilePath = "savedData.dat";
+        private Context context = null;
+        private readonly SaveData savedata;
+        private readonly bool IsInitializing;
 
         public MainWindow()
         {
+            IsInitializing = true;
             InitializeComponent();
-            savedata= savedata.GetSaveData();
+            savedata= SaveData.GetSaveData();
             TokenTextEdit.Text = savedata.Token+"";
             MoneyLimit.Text = savedata.MoneyLimitValue + "";
             USDRadioButton.IsChecked = savedata.Currency == Currency.Usd;
@@ -50,14 +45,8 @@ namespace Tinkoff
             catch (Exception)
             {
                 ErrorTextBlock.Text = "Неправильный токен";
-            }            
-            
+            }
             IsInitializing = false;
-        }
-
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -69,9 +58,9 @@ namespace Tinkoff
                 savedata.Save();
                 ErrorTextBlock.Text = "";
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-               
+                MessageBox.Show($"При изменении лимита произошла ошибка: {ex.Message}");
             }
         }
 
@@ -91,14 +80,13 @@ namespace Tinkoff
             }
         }
 
-        Context GetContext(string token)
+        private Context GetContext(string token)
         {
             try
             {
                 var connection = ConnectionFactory.GetConnection(token);
                 ErrorTextBlock.Text = "";
                 return connection.Context;
-               
             }
             catch (Exception)
             {
@@ -107,68 +95,98 @@ namespace Tinkoff
             }
         }
 
-  
         private async Task GetPriceChange(Context context, Currency curr, DateTime startDate, DateTime endDate)
         {
-            if (startDate > endDate) 
+            if (startDate > endDate)
             { ErrorTextBlock.Text = "Первая дата больше второй"; return; }
 
             CandleInterval interval;
             if ((endDate - startDate).TotalDays <=7) interval = CandleInterval.Hour;
-                else  if ((endDate - startDate).TotalDays <= 30) interval = CandleInterval.Day;
+                else  if ((endDate - startDate).TotalDays <= 90) interval = CandleInterval.Day;
                         else  interval = CandleInterval.Week;
             try
             {
                 ErrorTextBlock.Text = "Идет загрузка";
                 decimal priceLimit = savedata.MoneyLimitValue;
                 MarketInstrumentList markertlist = await context.MarketStocksAsync();
-                Dictionary<string, decimal> dif = new Dictionary<string, decimal>();
+                DataTable diff = new DataTable();
+                diff.Columns.Add("Name");
+                diff.Columns.Add("Change", typeof(decimal));
+                diff.Columns.Add("Линейность");
+                diff.Columns.Add("Zacks рейтинг");
+                int failedInstrumentCount = 0;
                 foreach (var instrument in markertlist.Instruments.Where((i) => i.Currency == curr))
                 {
-                   
                     try
                     {
-                        List<CandlePayload> candles= ( await context.MarketCandlesAsync(instrument.Figi, 
-                            DateTime.SpecifyKind( startDate,DateTimeKind.Local), DateTime.SpecifyKind(endDate,DateTimeKind.Local), 
-                            interval)).Candles;
-                       // List<CandlePayload> candles= ( await context.MarketCandlesAsync(instrument.Figi, DateTime.Now.AddDays(-2), DateTime.Now, CandleInterval.Hour)).Candles;
+                        Thread.Sleep(100);
+                        List<CandlePayload> candles = (await context.MarketCandlesAsync(instrument.Figi, DateTime.SpecifyKind(startDate, DateTimeKind.Local), DateTime.SpecifyKind(endDate, DateTimeKind.Local), interval)).Candles;
                         if (candles.Count > 0 && candles.Last().Close < priceLimit)
-                            dif[instrument.Name] = Math.Round((candles[0].Open - candles.Last().Close) / candles.Last().Close * 100,2)*-1;
-                        candles= (await context.MarketCandlesAsync(instrument.Figi, DateTime.Today, DateTime.Today.AddDays(0.99),
-                            CandleInterval.Minute)).Candles;
-                        File.WriteAllText("data.txt", $"{instrument.Name} - H={Hurst.CalculateHurst(candles)}");
+                        {
+
+                            string name = instrument.Name;
+                            decimal growth = Math.Round((candles[0].Open - candles.Last().Close) / candles.Last().Close * 100, 2) * -1;
+                            decimal linearity = GetMadeUpCoeff(candles);
+                            string zacksScore = linearity<(decimal)0.1 && curr!=Currency.Rub && growth>0? await GetZacksScore(instrument.Ticker):"";
+                            diff.Rows.Add(name, growth, $"{linearity:#0.##%}",zacksScore);
+                        }
                     }
-                    catch (Exception e)
+                    catch (Exception)
                     {
-                        continue;
+                        failedInstrumentCount++;
                     }
                 }
-                var listofDifs = dif.ToList();
-                listofDifs.Sort((el1, el2) => el2.Value.CompareTo(el1.Value));
-                //foreach (var item in listofDifs)
-               // {
-                    DataDataGrid.ItemsSource= listofDifs;
-                    //listListBox.Items.Add($"{item.Key}  {(item.Value * -1).ToString("n2")}%");
-               // }
-                   
-                ErrorTextBlock.Text = "";
+                DataDataGrid.ItemsSource = diff.DefaultView;
+                ErrorTextBlock.Text = $"Всего {diff.Rows.Count+ failedInstrumentCount} акций \nПоказано {diff.Rows.Count} акций\nЗафейлилось {failedInstrumentCount} акций";
             }
             catch (Exception e)
             {
                 ErrorTextBlock.Text=e.Message;
             }
+        }
+
+        private decimal GetMadeUpCoeff(List<CandlePayload> candles)
+        {
+            decimal k = (candles.Last().Close - candles.First().Close) / candles.Count;
+            decimal b = candles.First().Close;
+            List<decimal> diffs = new List<decimal>();
+            for (int x = 1; x <= candles.Count; x++)
+            {
+                diffs.Add(Math.Abs(candles[x - 1].Close-(k * x+b))/ (k * x + b));
+            }
+            return diffs.Average();
+        }
+
+        private async Task<string> GetZacksScore(string secCode)
+        {
+            MatchCollection matches;
+            using (WebClient client = new WebClient())
+            {
+                string htmlCode = await client.DownloadStringTaskAsync($"https://zacks.com/stock/quote/{secCode}");
+                matches = Regex.Matches(htmlCode, " <p class=\"rank_view\">\\n\\s*(\\d.*?)<span");
+            }
+
+            if (matches.Count > 0 )
+            {
+                if(matches[0].Groups.Count>1)
+                    return matches[0].Groups[1].Value;
+                
+            }
+            return string.Empty;
             
+
+
         }
 
         [Serializable]
-        class SaveData
+        private class SaveData
         {
             public string Token { get; set; }
             public decimal MoneyLimitValue { get; set; }
             public Currency Currency { get; set; }
             public DateTime StartDate { get; set; }
             public DateTime EndDate { get; set; }
-            public SaveData GetSaveData()
+            public static SaveData GetSaveData()
             {
                 try
                 {
@@ -183,6 +201,7 @@ namespace Tinkoff
                     return new SaveData();
                 }
             }
+
             public void Save()
             {
                 using (FileStream fs = File.OpenWrite(saveFilePath))
@@ -207,19 +226,11 @@ namespace Tinkoff
             savedata.Save();
         }
 
-        private void USDRadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-            if (IsInitializing) return;
-            savedata.Currency = RubRadioButton?.IsChecked == true ? Currency.Rub : Currency.Usd;
-            savedata.Save();
-        }
-
         private void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
         {
             if (IsInitializing) return;
             savedata.StartDate = StartDate.SelectedDate.GetValueOrDefault();
             savedata.Save();
-
         }
 
         private void EndDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
@@ -227,7 +238,6 @@ namespace Tinkoff
             if (IsInitializing) return;
             savedata.EndDate = ((DateTime)e.AddedItems[0]).AddDays(1) > DateTime.Now ? DateTime.Now : ((DateTime)e.AddedItems[0]).AddDays(1);
             savedata.Save();
-
         }
     }
 }
