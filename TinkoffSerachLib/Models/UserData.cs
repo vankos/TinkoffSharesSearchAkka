41c1561bb3f100206a0516b8dd67b6a03702e4a9
Vanko <vano111ru@yandex.ru>
2021-10-02 17:41:08 +0300

    исправлен механизм вызова методов по изменению значений текстовых контролов данные теперь сохраняются в джейсоне

--- TinkoffSerachLib/Models/UserData.cs
+++ TinkoffSerachLib/Models/UserData.cs
@@ -6,12 +6,35 @@ namespace TinkoffSerachLib.Models
     [Serializable]
     public class UserData
     {
-        public string Token { get; set; }
-        public decimal MoneyLimitValue { get; set; }
+        private decimal moneyLimitValue;
+        private decimal linearity;
+        
         public Currency Currency { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        public decimal Linearity { get; set; }
-        
+        public string Token { get; set; }
+        public decimal MoneyLimit
+        {
+            get { return moneyLimitValue; }
+            set 
+            { 
+                moneyLimitValue = value;
+                OnMoneyLimitValueChanged?.Invoke(this, value);
+            }
+        }
+
+        public decimal Linearity
+        {
+            get { return linearity; }
+            set 
+            { 
+                linearity = value;
+                OnLinearityChanged?.Invoke(this, value);
+            }
+        }
+
+        public event EventHandler<decimal> OnMoneyLimitValueChanged;
+        public event EventHandler<decimal> OnLinearityChanged;
+
     }
 }
