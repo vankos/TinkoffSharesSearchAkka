0c5607268aca26bbd7dce691f67435261a7b6922
Vanko <vano111ru@yandex.ru>
2021-10-19 21:49:30 +0300

    добавлена обработка необработанных исключений

--- WPFCoreUI/WPFCoreUI/App.xaml.cs
+++ WPFCoreUI/WPFCoreUI/App.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace WPFCoreUI
 {
@@ -13,5 +7,10 @@ namespace WPFCoreUI
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show($"Возникла неоработанная ошибка:\n{e.Exception.Message}", "Необработанная ошибка", MessageBoxButton.OK);
+            e.Handled = true;
+        }
     }
 }
